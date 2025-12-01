216343abc08ff6f19d0d85692b131d63c79889d7
Sebastian Stupar <stuparsebastian@gmail.com>
2025-12-01 22:58:18 +0100

    fix: Extend system dependencies for weasyprint

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Instalează dependențele de sistem necesare pentru weasyprint
-apt-get update && apt-get install -y libpango-1.0-0 libpangoft2-1.0-0
+# Lista a fost extinsă pentru a include pachete pentru grafică și imagini.
+apt-get update && apt-get install -y \
+    libpango-1.0-0 \
+    libpangocairo-1.0-0 \
+    libgdk-pixbuf2.0-0
