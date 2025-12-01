707a6f7c4b5f18492e9b49bd40b65755712a650f
Sebastian Stupar <stuparsebastian@gmail.com>
2025-12-01 23:14:14 +0100

    chore: Remove unused build.sh file

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Instalează dependențele de sistem necesare pentru weasyprint
-# Lista a fost extinsă pentru a include pachete pentru grafică și imagini.
-apt-get update && apt-get install -y \
-    libpango-1.0-0 \
-    libpangocairo-1.0-0 \
-    libgdk-pixbuf2.0-0
